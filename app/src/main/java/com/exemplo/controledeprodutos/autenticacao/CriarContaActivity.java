dc8fb4e9797d67d88eca101525f2ed0c17ed552b
Estelson Medeiros Pereira <estelson@gmail.com>
2022-04-12 20:31:30 -0300

    94. Validando Tela de Cadastro

--- app/src/main/java/com/exemplo/controledeprodutos/autenticacao/CriarContaActivity.java
+++ app/src/main/java/com/exemplo/controledeprodutos/autenticacao/CriarContaActivity.java
@@ -3,14 +3,69 @@ package com.exemplo.controledeprodutos.autenticacao;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.exemplo.controledeprodutos.R;
 
 public class CriarContaActivity extends AppCompatActivity {
 
+    private EditText edit_nome;
+    private EditText edit_email;
+    private EditText edit_senha;
+
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criar_conta);
+
+        iniciarComponentes();
+
+        configCliques();
     }
+
+    private void iniciarComponentes() {
+        edit_nome = findViewById(R.id.edit_nome);
+        edit_email = findViewById(R.id.edit_email);
+        edit_senha = findViewById(R.id.edit_senha);
+        progressBar = findViewById(R.id.progressBar);
+
+        TextView text_titulo = findViewById(R.id.text_titulo);
+        text_titulo.setText("Criar conta");
+    }
+
+    public void validarDados(View view) {
+        String nome = edit_nome.getText().toString();
+        String email = edit_email.getText().toString();
+        String senha = edit_senha.getText().toString();
+
+        if(!nome.isEmpty()) {
+            if(!email.isEmpty()) {
+                if(!senha.isEmpty()) {
+                    Toast.makeText(this, "Tudo certo!", Toast.LENGTH_SHORT).show();
+                } else {
+                    edit_senha.requestFocus();
+                    edit_senha.setError("Informe sua senha");
+                }
+            } else {
+                edit_email.requestFocus();
+                edit_email.setError("Informe seu e-Mail");
+            }
+        } else {
+            edit_nome.requestFocus();
+            edit_nome.setError("Informe seu nome");
+        }
+    }
+
+    private void configCliques() {
+        findViewById(R.id.id_voltar).setOnClickListener(view -> {
+            finish();
+        });
+    }
+
 }
--- app/src/main/java/com/exemplo/controledeprodutos/autenticacao/LoginActivity.java
+++ app/src/main/java/com/exemplo/controledeprodutos/autenticacao/LoginActivity.java
@@ -2,15 +2,34 @@ package com.exemplo.controledeprodutos.autenticacao;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.exemplo.controledeprodutos.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView text_criar_conta;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        iniciarComponentes();
+
+        configCliques();
     }
+
+    private void iniciarComponentes() {
+        text_criar_conta = findViewById(R.id.text_criar_conta);
+    }
+
+    private void configCliques() {
+        text_criar_conta.setOnClickListener(view -> {
+            startActivity(new Intent(this, CriarContaActivity.class));
+        });
+    }
+
 }
