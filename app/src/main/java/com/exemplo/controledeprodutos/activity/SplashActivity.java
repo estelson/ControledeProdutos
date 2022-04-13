2ec28871548f4d26987ba81428e958efd890f8bf
Estelson Medeiros Pereira <estelson@gmail.com>
2022-04-13 15:31:49 -0300

    99. Criando Tela Splash Activity

---
+++ app/src/main/java/com/exemplo/controledeprodutos/activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.exemplo.controledeprodutos.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.exemplo.controledeprodutos.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
