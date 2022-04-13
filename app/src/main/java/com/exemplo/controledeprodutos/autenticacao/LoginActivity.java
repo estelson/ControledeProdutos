b911724222cbd3bc311e4060c98ca52d682172f5
Estelson Medeiros Pereira <estelson@gmail.com>
2022-04-12 21:32:25 -0300

    97. Validando Tela de Login

--- app/src/main/java/com/exemplo/controledeprodutos/autenticacao/LoginActivity.java
+++ app/src/main/java/com/exemplo/controledeprodutos/autenticacao/LoginActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.exemplo.controledeprodutos.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText edit_email;
+    private EditText edit_senha;
 
     private TextView text_criar_conta;
 
@@ -22,7 +27,27 @@ public class LoginActivity extends AppCompatActivity {
         configCliques();
     }
 
+    public void logar(View view) {
+        String email = edit_email.getText().toString().trim();
+        String senha = edit_senha.getText().toString();
+
+        if(!email.isEmpty()) {
+            if(!senha.isEmpty()) {
+
+            } else {
+                edit_senha.requestFocus();
+                edit_senha.setError("Informe sua senha");
+            }
+        } else {
+            edit_email.requestFocus();
+            edit_email.setError("Informe seu e-Mail");
+        }
+    }
+
     private void iniciarComponentes() {
+        edit_email = findViewById(R.id.edit_email);
+        edit_senha = findViewById(R.id.edit_senha);
+
         text_criar_conta = findViewById(R.id.text_criar_conta);
     }
 
