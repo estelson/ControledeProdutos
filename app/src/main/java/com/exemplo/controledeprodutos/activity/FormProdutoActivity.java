4f0292dc87e74dc78e7c49968dfa124e4b711410
Estelson Medeiros Pereira <estelson@gmail.com>
2022-04-15 23:00:28 -0300

    109. Configurando Permissão e Abrindo Galeria do Dispositivo

--- app/src/main/java/com/exemplo/controledeprodutos/activity/FormProdutoActivity.java
+++ app/src/main/java/com/exemplo/controledeprodutos/activity/FormProdutoActivity.java
@@ -2,16 +2,27 @@ package com.exemplo.controledeprodutos.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.exemplo.controledeprodutos.model.Produto;
 import com.exemplo.controledeprodutos.R;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.normal.TedPermission;
+
+import java.util.List;
 
 public class FormProdutoActivity extends AppCompatActivity {
 
+    private static final int REQUEST_GALERIA = 100;
+
+    private ImageView imagem_produto;
     private EditText edit_produto;
     private EditText edit_quantidade;
     private EditText edit_valor;
@@ -23,9 +34,7 @@ public class FormProdutoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_produto);
 
-        edit_produto = findViewById(R.id.edit_produto);
-        edit_quantidade = findViewById(R.id.edit_quantidade);
-        edit_valor = findViewById(R.id.edit_valor);
+        iniciarComponentes();
 
         Bundle bundle = getIntent().getExtras();
         if(bundle != null) {
@@ -33,6 +42,51 @@ public class FormProdutoActivity extends AppCompatActivity {
 
             editarProduto();
         }
+    }
+
+    private void iniciarComponentes() {
+        edit_produto = findViewById(R.id.edit_produto);
+        edit_quantidade = findViewById(R.id.edit_quantidade);
+        edit_valor = findViewById(R.id.edit_valor);
+        imagem_produto = findViewById(R.id.imagem_produto);
+    }
+
+    public void abrirGaleria(View view) {
+        verificarPermissaoGaleria();
+    }
+
+    private void abrirGaleria() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, REQUEST_GALERIA);
+    }
+
+    private void showDialogPermissao(PermissionListener listener, String[] permissoes) {
+        TedPermission.create()
+                .setPermissionListener(listener)
+                .setDeniedTitle("Permissões")
+                .setDeniedMessage("Você negou a permissão para acessar a galeria do dispositivo. Deseja permitir?")
+                .setDeniedCloseButtonText("Não")
+                .setGotoSettingButtonText("Sim")
+                .setPermissions(permissoes)
+                .check();
+    }
+
+    private void verificarPermissaoGaleria() {
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                abrirGaleria();
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+                Toast.makeText(FormProdutoActivity.this, "Permissão negada", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        showDialogPermissao(permissionListener, new String[] {
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        });
     }
 
     public void salvarProduto(View view) {
